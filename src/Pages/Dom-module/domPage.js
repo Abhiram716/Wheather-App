6fc5b5fdf4f40ad18a5eeb79fcad0b34718fda43
Abhiram <a4abhiram125@gmail.com>
2022-12-11 23:48:00 +0530

    updated src/

--- src/Pages/Dom-module/domPage.js
+++ src/Pages/Dom-module/domPage.js
@@ -5,8 +5,17 @@ const Humidity = document.querySelector(".humidity");
 const windSpeed = document.querySelector(".wind");
 const backgroundImage = document.body.style.backgroundImage;
 const Icon = document.querySelector(".icon");
+const searchBar = document.querySelector(".search-bar");
+const searchButton = document.querySelector(".search button");
 
-
-export{
-    city,Temp,Description,Humidity,windSpeed,backgroundImage,Icon
-}
+export {
+  city,
+  Temp,
+  Description,
+  Humidity,
+  windSpeed,
+  backgroundImage,
+  Icon,
+  searchBar,
+  searchButton,
+};
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,13 @@
-import {city,Temp,Description,Humidity,windSpeed,Icon} from"./Dom-module/domPage.js";
+import {
+  city,
+  Temp,
+  Description,
+  Humidity,
+  windSpeed,
+  Icon,
+  searchBar,
+  searchButton,
+} from "./Dom-module/domPage.js";
 
 let weather = {
   apiKey: "a6830eb4fc6bc1e54c02233909a6fe50",
@@ -12,35 +21,36 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
+
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
 
-  city.innerText= "Weather in " + name;
-  Icon.src ="https://openweathermap.org/img/wn/" + icon + ".png";
-  Temp.innerText = temp + "°C";
-  Description.innerText = description;
-  Humidity.innerText =
-    "Humidity: " + humidity + "%";
-  windSpeed.innerText = "Wind Speed: " + speed + "Kmph";
-  document.body.style.backgroundImage= "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    city.innerText = "Weather in " + name;
+    Icon.src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    Temp.innerText = temp + "°C";
+    Description.innerText = description;
+    Humidity.innerText = "Humidity: " + humidity + "%";
+    windSpeed.innerText = "Wind Speed: " + speed + "Kmph";
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
+
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
+searchButton.addEventListener("click", function () {
   weather.search();
 });
 
-document.querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+searchBar.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
 
 weather.fetchWeather("Delhi");
